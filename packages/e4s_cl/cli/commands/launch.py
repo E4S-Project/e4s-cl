46f499b9307d20e7d4d0f80cbc624fe75538eacf
Jean-Baptiste Skutnik <jb.skutnik@gmail.com>
2020-05-04 11:20:41 -0700

    Profile arguments support at launch

--- packages/e4s_cl/cli/commands/launch.py
+++ packages/e4s_cl/cli/commands/launch.py
@@ -5,14 +5,65 @@ launcher detection, profile loading, and subprocess creation.
 """
 
 import os
+from pathlib import Path
+from argparse import ArgumentTypeError, Namespace
 from e4s_cl.cf.launchers import LAUNCHERS, parse_cli
 from e4s_cl import EXIT_SUCCESS, E4S_CL_SCRIPT
 from e4s_cl import logger, util
 from e4s_cl.cli import arguments
 from e4s_cl.cli.command import AbstractCommand
+from e4s_cl.model.profile import Profile
 
 LOGGER = logger.get_logger(__name__)
 _SCRIPT_CMD = os.path.basename(E4S_CL_SCRIPT)
+
+def _argument_profile(string):
+    """Argument type callback.
+    Asserts the entered string matches a defined profile."""
+    profile = Profile.controller().one({'name': string})
+
+    if not profile:
+        raise ArgumentTypeError("Profile {} does not exist".format(string))
+    return profile
+
+def _argument_path(string):
+    """Argument type callback.
+    Asserts that the string corresponds to an existing path."""
+    return Path(string.strip()).as_posix()
+
+def _argument_path_comma_list(string):
+    """Argument type callback.
+    Asserts that the string corresponds to a list of existing paths."""
+    return [_argument_path(data) for data in string.split(',')]
+
+def _parameters(arguments):
+    """Generate compound parameters by merging profile and cli arguments
+    The profile's parameters have less priority than the ones specified on
+    the command line."""
+    if type(arguments) == Namespace:
+        arguments = vars(arguments)
+
+    parameters = dict(arguments.get('profile', {}))
+
+    for attr in ['image', 'backend', 'libraries', 'files']:
+        if arguments.get(attr, None):
+            parameters.update({attr: arguments[attr]})
+
+    return parameters
+
+def _format_execute(parameters):
+    from e4s_cl.cli.commands.execute import COMMAND as execute_cmd
+    execute_command = str(execute_cmd).split()
+
+    for attr in ['image', 'backend']:
+        if parameters.get(attr, None):
+            execute_command += ["--{}".format(attr), parameters[attr]]
+
+    for attr in ['libraries', 'files']:
+        if parameters.get(attr, None):
+            execute_command += ["--{}".format(attr), ",".join(parameters[attr])]
+
+    return execute_command
 
 class LaunchCommand(AbstractCommand):
     """``launch`` subcommand."""
@@ -20,13 +71,21 @@ class LaunchCommand(AbstractCommand):
     def _construct_parser(self):
         usage = "%s [arguments] [launcher] [launcher_arguments] [--] <command> [command_arguments]" % self.command
         parser = arguments.get_parser(prog=self.command, usage=usage, description=self.summary)
+        parser.add_argument('--profile',
+                            type=_argument_profile,
+                            help="Name of the profile to use",
+                            default={},
+                            metavar='profile')
         parser.add_argument('--image',
+                            type=_argument_path,
                             help="Container image to use",
                             metavar='image')
         parser.add_argument('--files',
+                            type=_argument_path_comma_list,
                             help="Files to bind, comma-separated",
                             metavar='files')
         parser.add_argument('--libraries',
+                            type=_argument_path_comma_list,
                             help="Libraries to bind, comma-separated",
                             metavar='libraries')
         parser.add_argument('--backend',
@@ -65,15 +124,7 @@ class LaunchCommand(AbstractCommand):
         args = self._parse_args(argv)
         launcher, program = LaunchCommand.parse_launcher_cmd(args.cmd)
 
-        execute_command = [E4S_CL_SCRIPT, 'execute']
-        if args.files:
-            execute_command += ['--files', args.files]
-        if args.libraries:
-            execute_command += ['--libraries', args.libraries]
-        if args.backend:
-            execute_command += ['--backend', args.backend]
-        if args.image:
-            execute_command += ["--image", args.image]
+        execute_command = _format_execute(_parameters(args))
 
         LOGGER.debug(" ".join(launcher + execute_command + program))
         util.create_subprocess_exp(launcher + execute_command + program)
