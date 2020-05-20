6eb85561384af590f8d9a67ada668e11b13cb5fe
Jean-Baptiste Skutnik <jb.skutnik@gmail.com>
2020-05-20 15:31:05 -0700

    Conditionnal enabling of Nvidia support in singularity

--- packages/e4s_cl/cf/containers/singularity.py
+++ packages/e4s_cl/cf/containers/singularity.py
@@ -4,14 +4,21 @@ from e4s_cl.cf.containers import Container
 
 LOGGER = logger.get_logger(__name__)
 
+
 class SingularityContainer(Container):
     def run(self, command, redirect_stdout=False):
         self.add_ld_library_path("/.singularity.d/libs")
-        self.env.update({'SINGULARITYENV_LD_PRELOAD': ":".join(self.ld_preload)})
-        self.env.update({'SINGULARITYENV_LD_LIBRARY_PATH': ":".join(self.ld_lib_path)})
+        self.env.update(
+            {'SINGULARITYENV_LD_PRELOAD': ":".join(self.ld_preload)})
+        self.env.update(
+            {'SINGULARITYENV_LD_LIBRARY_PATH': ":".join(self.ld_lib_path)})
         self.format_bound()
-        container_cmd = [which('singularity'), 'exec', '--nv', self.image.as_posix()] + command
-        _, output = create_subprocess_exp(container_cmd, env=self.env, redirect_stdout=redirect_stdout)
+        nvidia_flag = ['--nv'] if self._has_nvidia() else []
+        container_cmd = [which('singularity'), 'exec'
+                         ] + nvidia_flag + [self.image.as_posix()] + command
+        _, output = create_subprocess_exp(container_cmd,
+                                          env=self.env,
+                                          redirect_stdout=redirect_stdout)
         return output
 
     def format_bound(self):
@@ -27,6 +34,16 @@ class SingularityContainer(Container):
     @staticmethod
     def is_available():
         return which('singularity') is not None
+
+    def _has_nvidia(self):
+        _, output = create_subprocess_exp(['ldconfig', '-p'],
+                                          redirect_stdout=True)
+
+        if not 'nvidia' in output:
+            LOGGER.warning("Disabling Nvidia support: no libraries found")
+            return False
+        return True
+
 
 MIMES = ['simg']
 AVAILABLE = SingularityContainer.is_available()
