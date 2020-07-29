ce102ccc0f1ac7ddbc5bfcff02f8293f85e90d03
Jean-Baptiste Skutnik <jb.skutnik@gmail.com>
2020-07-29 12:01:08 -0700

    Added util tests

---
+++ packages/e4s_cl/tests/test_util.py
@@ -0,0 +1,22 @@
+import pathlib
+from e4s_cl import tests
+from e4s_cl.util import opened_files, which, ldd
+
+
+class UtilTest(tests.TestCase):
+    def test_opened_files(self):
+        returncode, files = opened_files(['cat', '/dev/null'])
+        self.assertEqual(returncode, 0)
+        self.assertIn('/dev/null', [path.as_posix() for path in files])
+
+    def test_which(self):
+        path = which('ls')
+        executable = pathlib.Path(path)
+        self.assertTrue(executable.exists())
+        self.assertTrue(executable.is_absolute())
+        return executable.as_posix()
+
+    def test_ldd(self):
+        binary = self.test_which()
+        libraries = ldd(binary)
+        self.assertTrue(libraries)
