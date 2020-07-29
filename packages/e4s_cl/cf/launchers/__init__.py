db9f357793a2a628dd134eaeaa1fc94bd385a9ec
Jean-Baptiste Skutnik <jb.skutnik@gmail.com>
2020-07-29 11:54:43 -0700

    Updated launcher modules

--- packages/e4s_cl/cf/launchers/__init__.py
+++ packages/e4s_cl/cf/launchers/__init__.py
@@ -2,19 +2,60 @@
 Entry point for supported program launchers"""
 
 import sys
+from pathlib import Path
+from importlib import import_module
 from os import environ
 from e4s_cl.util import walk_packages
 from e4s_cl.error import InternalError
 
-LAUNCHERS = []
+LAUNCHERS = {}
 
-for _, module_name, _ in walk_packages(__path__, prefix=""):
-    LAUNCHERS.append(module_name)
+
+class Parser(object):
+    def __init__(self, arguments):
+        self.arguments = arguments
+
+    def parse(self, command):
+        """
+        parse_cli: list[str] -> list[str], list[str]
+
+        Separate a command line into launcher and program.
+        """
+        position = 0
+        known = True
+        launcher = []
+
+        launcher.append(command[position])
+        position += 1
+
+        while known and position < len(command):
+            if command[position] not in self.arguments.keys():
+                known = False
+                break
+
+            to_skip = self.arguments[command[position]]
+
+            for index in range(0, to_skip + 1):
+                launcher.append(command[position + index])
+
+            position += (to_skip + 1)
+
+        return launcher, command[position:]
 
 
 def parse_cli(cmd):
     """Determine if the launcher is supported
     import its module and parse the command line"""
-    module = "{}.{}".format(__name__, cmd[0])
-    __import__(module)
-    return sys.modules[module].parse_cli(cmd)
+    script = Path(cmd[0]).name
+
+    module_name = LAUNCHERS.get(script)
+
+    if module_name:
+        return sys.modules[module_name].PARSER.parse(cmd)
+    raise NotImplementedError("Launcher %s is not supported" % script)
+
+
+for _, module_name, _ in walk_packages(path=__path__, prefix=__name__ + '.'):
+    import_module(name=module_name)
+    for script_name in sys.modules[module_name].SCRIPT_NAMES:
+        LAUNCHERS.update({script_name: module_name})
--- packages/e4s_cl/cf/launchers/mpirun.py
+++ packages/e4s_cl/cf/launchers/mpirun.py
@@ -1,58 +1,24 @@
-"""mpirun launcher support
+"""mpirun launcher support"""
 
-Defines the ``parse_cli`` command adapted to mpirun to interpret
-mpirun commands"""
+from e4s_cl.cf.launchers import Parser
 
-from e4s_cl import logger
+SCRIPT_NAMES = ['mpirun']
 
-LOGGER = logger.get_logger(__name__)
+ARGUMENTS = {
+    "-genv": 2,
+    "-genvlist": 1,
+    "-genvnone": 0,
+    "-genvall": 0,
+    "-f": 1,
+    "-hosts": 1,
+    "-wdir": 1,
+    "-configfile": 1,
+    "-env": 2,
+    "-envlist": 1,
+    "-envnone": 0,
+    "-envall": 0,
+    "-n": 1,
+    "-np": 1
+}
 
-ARGUMENTS = {"-genv": 2,
-             "-genvlist": 1,
-             "-genvnone": 0,
-             "-genvall": 0,
-             "-f": 1,
-             "-hosts": 1,
-             "-wdir": 1,
-             "-configfile": 1}
-
-LOCAL_ARGUMENTS = {"-env": 2,
-                   "-envlist": 1,
-                   "-envnone": 0,
-                   "-envall": 0,
-                   "-n": 1,
-                   "-np": 1}
-
-def parse_cli(cmd):
-    """
-    parse_cli: list[str] -> list[str], list[str]
-
-    Separate a command line into launcher and program.
-    """
-    position = 0
-    known = True
-    launcher = []
-
-    full_list = ARGUMENTS
-    full_list.update(LOCAL_ARGUMENTS)
-
-    if cmd[position] != 'mpirun':
-        return [], cmd
-
-    launcher.append(cmd[position])
-    position += 1
-
-    while known and position < len(cmd):
-        LOGGER.debug("Evaluating %s", cmd[position])
-        if cmd[position] not in full_list.keys():
-            known = False
-            break
-
-        to_skip = full_list[cmd[position]]
-
-        for index in range(0, to_skip+1):
-            launcher.append(cmd[position+index])
-
-        position += (to_skip + 1)
-
-    return launcher, cmd[position:]
+PARSER = Parser(ARGUMENTS)
