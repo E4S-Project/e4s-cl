219bd8ec830bd5428f19a7a634088f9f426a3df9
Jean-Baptiste Skutnik <jb.skutnik@gmail.com>
2020-07-29 17:39:03 -0700

    Added execute tests

---
+++ packages/e4s_cl/cli/commands/tests/test_execute.py
@@ -0,0 +1,42 @@
+from e4s_cl import tests
+from e4s_cl.util import host_libraries
+from e4s_cl.cli.commands.execute import COMMAND
+
+
+class ExecuteTest(tests.TestCase):
+    def test_simple(self):
+        argv = ['--backend', 'bash', '--image', '/dev/null', 'ls']
+        stdout, stderr = self.assertCommandReturnValue(0, COMMAND, argv)
+
+    def test_nonexistent(self):
+        argv = ['--backend', 'neverImplemented', '--image', '/dev/null', 'ls']
+        self.assertNotCommandReturnValue(0, COMMAND, argv)
+        argv = ['--backend', 'bash', '--image', '/this/does/not/exist', 'ls']
+        self.assertNotCommandReturnValue(0, COMMAND, argv)
+
+    def test_libraries(self):
+        libraries = []
+        for soname in filter(lambda x: 'libc.so' in x,
+                             host_libraries().keys()):
+            libraries.append(host_libraries()[soname])
+        argv = ['--backend', 'bash', '--image', '/dev/null', '--libraries'
+                ] + libraries + ['ls']
+        self.assertCommandReturnValue(0, COMMAND, argv)
+
+        argv = ['--backend', 'bash', '--image', '/dev/null', '--libraries'
+                ] + ['/tmp' + lib for lib in libraries] + ['ls']
+        self.assertNotCommandReturnValue(0, COMMAND, argv)
+
+    def test_files(self):
+        files = [
+            '/etc/fstab', '/etc/group', '/etc/hosts', '/etc/hostname',
+            '/etc/localtime', '/etc/passwd'
+        ]
+
+        argv = ['--backend', 'bash', '--image', '/dev/null', '--files'
+                ] + files + ['ls']
+        self.assertCommandReturnValue(0, COMMAND, argv)
+
+        argv = ['--backend', 'bash', '--image', '/dev/null', '--files'
+                ] + ['/tmp' + f for f in files] + ['ls']
+        self.assertNotCommandReturnValue(0, COMMAND, argv)
