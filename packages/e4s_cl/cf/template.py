8986f1b3c154550c0400e8e24ffef794f11543f1
Jean-Baptiste Skutnik <jb.skutnik@gmail.com>
2020-11-06 13:13:01 -0800

    Added template module

---
+++ packages/e4s_cl/cf/template.py
@@ -0,0 +1,35 @@
+import os, stat
+import tempfile
+
+TEMPLATE = """#!/bin/bash -i # interactive for spack commands
+               # see https://github.com/spack/spack/issues/11098
+
+%s
+
+# Affirm the urgency of using the host libraries, as spack may override
+# this variable
+export LD_LIBRARY_PATH=%s:$LD_LIBRARY_PATH
+
+%s
+"""
+
+def setUp(command, libdir, setup=None):
+    if setup:
+        setup_line = "source %s" % setup
+    else:
+        setup_line = ''
+
+    if isinstance(command, list):
+        command = " ".join(command)
+
+    script = tempfile.NamedTemporaryFile('w', delete=False)
+    script.write(TEMPLATE % (setup_line, libdir, command))
+    script.close()
+
+    os.chmod(script.name, stat.S_IXUSR)
+
+    return script.name
+
+def tearDown(file_name):
+    if os.path.exists(file_name):
+        os.unlink(file_name)
