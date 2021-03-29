7ebf2d633644a321b998046f08974946418ee479
Jean-Baptiste Skutnik <jskutnik@uoregon.edu>
2021-03-29 12:26:04 -0700

    Revamped entrypoint script - library path set last minute

--- packages/e4s_cl/cf/template.py
+++ packages/e4s_cl/cf/template.py
@@ -1,6 +1,14 @@
+"""
+This file contains a BASH script template that will get written in the
+container. This script is to be the entrypoint as it should set the
+execution environment and allows for arbitrary code execution (e.g.
+library loading)
+"""
+
 import os, stat
 import tempfile
 from e4s_cl import logger
+from pathlib import Path
 
 LOGGER = logger.get_logger(__name__)
 
@@ -8,37 +16,81 @@ TEMPLATE = """#!/bin/bash -i
 # The shell need to be interactive for spack commands
 # see https://github.com/spack/spack/issues/11098
 
-%s
+# Source a user-provided script for convenience
+%(source_script)s
 
-# Affirm the urgency of using the host libraries, as spack may override
-# this variable
-export LD_LIBRARY_PATH=%s:$LD_LIBRARY_PATH
+# Enable the host's libraries for this last command by prepending the
+# resulting LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=%(library_dir)s${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
-%s
+# If a linker has been imported, this line will make sure it is loaded instead
+# of the default. This is of the utmost importance, as libc imports will break
+# with non-adapted linkers.
+export LD_PRELOAD=%(linker)s${LD_PRELOAD:+:${LD_PRELOAD}}
+
+# Finally, run the command
+%(command)s
 """
 
 
-def setUp(command, libdir, setup=None):
-    if setup:
-        setup_line = "source %s" % setup
-    else:
-        setup_line = ''
+class Entrypoint:
+    def __init__(self):
+        # Command to run in the container
+        self.command = []
 
-    if isinstance(command, list):
-        command = " ".join(command)
+        # Script to source before running anything
+        self.source_script_path = ''
 
-    script = tempfile.NamedTemporaryFile('w', delete=False)
-    script.write(TEMPLATE % (setup_line, libdir, command))
-    script.close()
+        # Path to a directory where the host libraries were bound
+        self.library_dir = ''
 
-    os.chmod(script.name, 0o755)
+        # Path to the imported host linker
+        self.linker = ''
 
-    LOGGER.debug("Running templated script:\n" + TEMPLATE, setup_line, libdir,
-                 command)
+    @property
+    def command(self):
+        return " ".join(self.__command)
 
-    return script.name
+    @command.setter
+    def command(self, rhs):
+        if isinstance(rhs, str):
+            self.__command = shlex.split(rhs)
+        elif isinstance(rhs, list):
+            self.__command = rhs
+        else:
+            raise InternalError("Invalid format for entrypoint command: %s" %
+                                repr(rhs))
 
+    @property
+    def source_script(self):
+        if self.source_script_path:
+            return ". %s" % self.source_script_path
+        return ""
 
-def tearDown(file_name):
-    if os.path.exists(file_name):
-        os.unlink(file_name)
+    def __str__(self):
+        fields = {
+            'source_script': self.source_script,
+            'command': self.command,
+            'library_dir': self.library_dir,
+            'linker': self.linker
+        }
+
+        return TEMPLATE % fields
+
+    def setUp(self):
+        script = tempfile.NamedTemporaryFile('w', delete=False)
+        script.write(str(self))
+        script.close()
+
+        os.chmod(script.name, 0o755)
+
+        LOGGER.debug("Running templated script:\n" +
+                     "".join('=' for _ in range(80)) + "\n%s\n" % str(self) +
+                     "".join('=' for _ in range(80)))
+
+        self.file_name = script.name
+        return self.file_name
+
+    def tearDown(self):
+        if getattr(self, 'file_name', False):
+            os.unlink(self.file_name)
