264072cf56c776615bd267b567e4e58fa3c97752
Jean-Baptiste Skutnik <jskutnik@uoregon.edu>
2021-04-01 16:40:12 -0700

    Limited HOST_LIBRARIES to 64 bits

--- packages/e4s_cl/cf/libraries.py
+++ packages/e4s_cl/cf/libraries.py
@@ -118,25 +118,42 @@ HOST_LIBRARIES = {}
 def host_libraries():
     """
     Output a dict containing all the libraries available on the host,
-    under the format {soname: path}
+    in x86_64 format, under the format {soname: path}
     """
     global HOST_LIBRARIES
 
     if HOST_LIBRARIES:
         return HOST_LIBRARIES
 
-    ldconfig_path = util.which('ldconfig')
+    # The versions that appear in `ldconfig -p`
+    # For some reason, ppc shows '64bits'
+    valid_versions = {'64bits', 'x86-64'}
 
+    ldconfig_path = util.which('ldconfig')
     if ldconfig_path is None:
         return HOST_LIBRARIES
 
-    _, output = create_subprocess_exp([ldconfig_path, '-p'],
-                                      redirect_stdout=True)
+    retval, output = create_subprocess_exp([ldconfig_path, '-p'],
+                                           redirect_stdout=True)
+
+    if retval:
+        raise InternalError("Error getting libraries using %s", ldconfig_path)
 
     for row in output.strip().split('\n')[1:]:
-        # Expecting format "libname.so.y (lib,arch) => /path/libname.so.y"
-        components = row.strip().split(' ')
-        HOST_LIBRARIES[components[0]] = components[-1]
+        # Expecting format "\t\tlibname.so.y (libc,arch) => /path/libname.so.y"
+        pattern = '^\s+(?P<soname>\S+(\.\S+)+).*\((?P<details>.*)\).*?(?P<path>(\/\S+)+)$'
+
+        m = re.match(pattern, row)
+        if not m:
+            continue
+
+        # Check the `arch` from above
+        # Sometimes there is no data, so handle that case
+        details = m.group('details').split(',')
+        if len(details) < 2 or 'OS' in details[1]:
+            continue
+
+        HOST_LIBRARIES[m.group('soname')] = m.group('path')
 
     return HOST_LIBRARIES
 
@@ -322,6 +339,7 @@ class LibrarySet(set):
 
         return trees
 
+
 def ELFDataFromDict(obj):
     out = ELFData()
 
@@ -329,5 +347,6 @@ def ELFDataFromDict(obj):
         setattr(out, key, value)
 
     return out
+
 
 JSON_HOOKS['ELFData'] = ELFDataFromDict
