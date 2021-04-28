229e527d38e8afd93278c80ee9c666feb2c8feab
Jean-Baptiste Skutnik <jskutnik@uoregon.edu>
2021-04-27 17:21:05 -0700

    Used functool.cache on ldconfig wrapper

--- packages/e4s_cl/cf/libraries/ldcache.py
+++ packages/e4s_cl/cf/libraries/ldcache.py
@@ -1,49 +1,53 @@
+"""
+Method using the ldconfig utility to get a list of libraries present on the host
+"""
+
 import re
+from functools import cache
+from e4s_cl import logger
 from e4s_cl.util import which, create_subprocess_exp
-from e4s_cl.error import InternalError
 
-# Dict with the host libraries, with sonames as keys, and paths as values
-__HOST_LIBRARIES = {}
+LOGGER = logger.get_logger(__name__)
 
 
+@cache
 def host_libraries():
     """
     Output a dict containing all the host's linker cache in x86_64 format
     under the format {soname: path}
     """
-    global __HOST_LIBRARIES
-
-    if __HOST_LIBRARIES:
-        return __HOST_LIBRARIES
-
     # The versions that appear in `ldconfig -p`
     # For some reason, ppc shows '64bits'
     valid_versions = {'64bits', 'x86-64'}
 
     ldconfig_path = which('ldconfig')
     if ldconfig_path is None:
-        return __HOST_LIBRARIES
+        LOGGER.error("ldconfig executable not found")
+        return {}
 
     retval, output = create_subprocess_exp([ldconfig_path, '-p'],
                                            redirect_stdout=True)
 
     if retval:
-        raise InternalError("Error getting libraries using %s", ldconfig_path)
+        LOGGER.error("Error getting libraries using %s", ldconfig_path)
+        return {}
+
+    _cache = {}
 
     for row in output.strip().split('\n')[1:]:
         # Expecting format "\t\tlibname.so.y (libc,arch) => /path/libname.so.y"
-        pattern = '^\s+(?P<soname>\S+(\.\S+)+).*\((?P<details>.*)\).*?(?P<path>(\/\S+)+)$'
+        pattern = r'^\s+(?P<soname>\S+(\.\S+)+).*\((?P<details>.*)\).*?(?P<path>(\/\S+)+)$'
 
-        m = re.match(pattern, row)
-        if not m:
+        match = re.match(pattern, row)
+        if not match:
             continue
 
         # Check the `arch` from above
         # Sometimes there is no data, so handle that case
-        details = m.group('details').split(',')
-        if len(details) < 2 or 'OS' in details[1]:
+        details = match.group('details').split(',')
+        if len(details) < 2 or details[1] not in valid_versions:
             continue
 
-        __HOST_LIBRARIES[m.group('soname')] = m.group('path')
+        _cache[match.group('soname')] = match.group('path')
 
-    return __HOST_LIBRARIES
+    return _cache
