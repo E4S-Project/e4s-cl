191335e485a6f1f0923ca9a86ac085194930f23c
Jean-Baptiste Skutnik <jskutnik@uoregon.edu>
2021-05-03 09:16:22 -0700

    Updated profile detect to use cf.libraries

--- packages/e4s_cl/cli/commands/profile/detect.py
+++ packages/e4s_cl/cli/commands/profile/detect.py
@@ -2,16 +2,13 @@
 Launch an executable and catch syscalls to determine a list of files. Output it to a profile if asked to.
 """
 
-import json
-import pathlib
-import os
 import re
+from json import JSONDecodeError
 from e4s_cl import EXIT_SUCCESS, EXIT_FAILURE, E4S_CL_SCRIPT, logger
 from e4s_cl.variables import is_master
-from e4s_cl.util import which, opened_files, create_subprocess_exp, flatten
-from e4s_cl.cf.libraries import host_libraries, ldd
+from e4s_cl.util import opened_files, create_subprocess_exp, flatten, json_dumps, json_loads
+from e4s_cl.cf.libraries import is_elf, resolve
 from e4s_cl.cf.launchers import interpret
-from e4s_cl.error import UniqueAttributeError
 from e4s_cl.cli import arguments
 from e4s_cl.model.profile import Profile
 from e4s_cl.cli.cli_view import AbstractCliView
@@ -19,26 +16,17 @@ from e4s_cl.cli.cli_view import AbstractCliView
 LOGGER = logger.get_logger(__name__)
 
 
-def filter_files(path_list, ldd_requirements):
+def filter_files(path_list):
     """
-    Filter a list of files from syscalls into categories
+    Categorize paths into libraries or files
+
+    Libraries are resolved with the linker and are to be imported in a special
+    location. They can only be ELF files.
+
+    Files are referenced using their paths and have to be imported at the same
+    location. They can be ELF objects that are dynamically loaded by the library.
     """
-    libraries, paths = [], []
-
-    # As in `man ld.so`
-    linker_default_paths = [
-        pathlib.Path('/lib'),
-        pathlib.Path('/lib64'),
-        pathlib.Path('/usr/lib'),
-        pathlib.Path('/usr/lib64')
-    ]
-
-    # Cut a:b:c in a Path list if set
-    linker_env_paths = [
-        pathlib.Path(dir)
-        for dir in filter(lambda x: x,
-                          os.environ.get('LD_LIBRARY_PATH', '').split(':'))
-    ]
+    libraries, files = set(), set()
 
     for path in path_list:
         try:
@@ -47,29 +35,20 @@ def filter_files(path_list, ldd_requirements):
         except PermissionError:
             continue
 
-        # Discard the linker cache
-        if path.name == 'ld.so.cache':
+        # Process shared objects
+        if is_elf(path):
+            if resolve(path):
+                # The library is resolved by the linker, treat it as a library
+                libraries.add(path.as_posix())
+                LOGGER.debug("File %s is a library", path.name)
+            else:
+                # It is a library BUT must be imported with a full path
+                files.add(path.as_posix())
+                LOGGER.debug("File %s is a library (non-standard)", path.name)
             continue
 
-        # Process shared objects
-        if re.match(r".*\.so.*", path.name):
-            if path.name in host_libraries().keys():
-                # The library is in the cache, it can be found using a soname
-                libraries.append(path.as_posix())
-                LOGGER.debug("File %s is a library (cache)", path.name)
-            elif path.name in ldd_requirements.keys() \
-                and ldd_requirements[path.name].get('found'):
-                # The library is not in the cache, but still found by the linker
-                libraries.append(path.as_posix())
-                LOGGER.debug("File %s is a library (found)", path.name)
-            elif path.parent in linker_default_paths + linker_env_paths:
-                # If not in the cache, but still found by the linker
-                libraries.append(path.as_posix())
-                LOGGER.debug("File %s is a library (default)", path.name)
-            else:
-                # Not standard, it is a library BUT must be imported with a full path
-                paths.append(path.as_posix())
-                LOGGER.debug("File %s is a library (non-standard)", path.name)
+        # Discard the linker cache, opened by default for every binary
+        if path.name == 'ld.so.cache':
             continue
 
         # Process files
@@ -78,19 +57,20 @@ def filter_files(path_list, ldd_requirements):
         for expr in blacklist:
             if not filtered and re.match("^%s.*" % expr, path.as_posix()):
                 filtered = True
+                break
 
         if not filtered:
-            paths.append(path.as_posix())
+            files.add(path.as_posix())
             LOGGER.debug("File %s is a regular file (non-blacklisted)",
                          path.name)
 
-    return libraries, paths
+    return libraries, files
 
 
 class ProfileDetectCommand(AbstractCliView):
     """``profile create`` subcommand."""
     def _construct_parser(self):
-        usage = "%s" % self.command
+        usage = "%s [-p profile] <mpi_launcher command>" % self.command
         parser = arguments.get_parser(prog=self.command,
                                       usage=usage,
                                       description=self.summary)
@@ -114,10 +94,9 @@ class ProfileDetectCommand(AbstractCliView):
             return EXIT_FAILURE
 
         launcher, program = interpret(args.cmd)
-        ldd_requirements = {}
 
         if launcher:
-            # If a launcher is present, create subprocesses and aggregate the results
+            # If a launcher is present, act as a launcher
             returncode, json_data = create_subprocess_exp(
                 launcher + [E4S_CL_SCRIPT, "--slave", "profile", "detect"] +
                 program,
@@ -128,10 +107,10 @@ class ProfileDetectCommand(AbstractCliView):
 
                 for line in json_data.split('\n'):
                     try:
-                        data = json.loads(line)
+                        data = json_loads(line)
                         file_paths.append(data['files'])
                         library_paths.append(data['libraries'])
-                    except json.JSONDecodeError:
+                    except JSONDecodeError:
                         pass
 
                 files = list(set(flatten(file_paths)))
@@ -140,10 +119,7 @@ class ProfileDetectCommand(AbstractCliView):
         else:
             # No launcher, analyse the command
             returncode, accessed_files = opened_files(args.cmd)
-            base_command = which(args.cmd[0])
-            if base_command:
-                ldd_requirements = ldd(base_command)
-            libs, files = filter_files(accessed_files, ldd_requirements)
+            libs, files = filter_files(accessed_files)
 
         if returncode:
             if is_master():
@@ -151,10 +127,10 @@ class ProfileDetectCommand(AbstractCliView):
             return EXIT_FAILURE
 
         # There are two cases: this is a master process, in which case the output
-        # must be processed, or this is a slave process, where we just print it all on stdout
-        # in a format the master will understand
+        # must be processed, or this is a slave process, where we just print it
+        # all on stdout in a format the master process will understand
         if not is_master():
-            print(json.dumps({
+            print(json_dumps({
                 'files': files,
                 'libraries': libs,
             }))
@@ -169,8 +145,8 @@ class ProfileDetectCommand(AbstractCliView):
             if not profile:
                 try:
                     profile = controller.create(identifier)
-                except Exception as e:  #TODO check what errors can be handled here
-                    LOGGER.debug(str(e))
+                except Exception as err:  #TODO check what errors can be handled here
+                    LOGGER.debug(str(err))
                     LOGGER.error("Profile creation failed.")
                     return EXIT_FAILURE
         else:
@@ -186,8 +162,8 @@ class ProfileDetectCommand(AbstractCliView):
         data = {'libraries': libs, 'files': files}
         try:
             controller.update(data, identifier)
-        except Exception as e:  # TODO same as above
-            LOGGER.debug(str(e))
+        except Exception as err:  # TODO same as above
+            LOGGER.debug(str(err))
             LOGGER.error("Profile update failed.")
             return EXIT_FAILURE
 
