85414999e9111bea93c71fb63b6e42f11374ddff
Jean-Baptiste Skutnik <jskutnik@uoregon.edu>
2021-08-05 14:50:40 -0700

    Refactor JSON handler

--- packages/e4s_cl/cf/json_handler.py
+++ packages/e4s_cl/cf/json_handler.py
@@ -11,22 +11,19 @@ class JSONHandler(logging.StreamHandler):
     identifier = 'JSON_FORMATTED_RECORD'
 
     def emit(self, record):
+        data = {
+            'host': socket.gethostname(),
+            'name': record.name,
+            'levelno': record.levelno,
+            'process': record.process,
+            'created': record.created,
+            # We format the message now to avoid issues converting to JSON
+            'msg': record.getMessage(),
+            self.identifier: 1
+        }
+
         try:
-            template = {
-                'host': socket.gethostname(),
-                'name': record.name,
-                'levelno': record.levelno,
-                'process': record.process,
-                'created': record.created,
-                # We format the message now to avoid issues converting to JSON
-                'msg': record.getMessage(),
-                self.identifier: 1
-            }
-
-            message = json.dumps(template)
-            stream = self.stream
-
-            stream.write(message + self.terminator)
+            self.stream.write(json.dumps(data) + self.terminator)
             self.flush()
         except RecursionError:
             raise
