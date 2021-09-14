c8798b92596e647e0f9754c13b01c7d851d74324
Frederick Deny <denyfrederick@gmail.com>
2021-09-14 14:32:41 -0700

    add whitespace trimming

--- packages/e4s_cl/cli/commands/init.py
+++ packages/e4s_cl/cli/commands/init.py
@@ -131,6 +131,7 @@ def create_profile(args, metadata):
 
         filtered_buffer = list(filter(lambda x : x in version_buffer.value.decode("utf-8"), accepted_imp))
         profile_name=filtered_buffer[-1]
+        profile_name = profile_name.replace(" ","")
     else:
         profile_name = getattr(args, 'profile_name',
                            "default-%s" % util.hash256(json.dumps(metadata)))
