a4d0672ab12745c3c7ebec5dcbbb7ce10ea04e1f
Jean-Baptiste Skutnik <jskutnik@uoregon.edu>
2021-11-05 11:36:05 -0700

    Resilient download script

--- scripts/download_assets.py
+++ scripts/download_assets.py
@@ -1,6 +1,7 @@
 #!/bin/env python3
 
 import os
+import stat
 import sys
 import lzma
 import json
@@ -16,27 +17,26 @@ from e4s_cl import logger, USER_PREFIX
 
 LOGGER = logger.get_logger(__name__)
 
-if __name__ == '__main__':
-    if getattr(sys, 'frozen', False):
-        __file__ = sys.executable
+BINARY_DIR = Path(USER_PREFIX, "binaries").as_posix()
+PROFILE_DIR = Path(USER_PREFIX, "profiles").as_posix()
+BINARY_INDEX = Path(BINARY_DIR, "index.json").as_posix()
+PROFILE_INDEX = Path(PROFILE_DIR, "index.json").as_posix()
 
-    if len(sys.argv) < 3:
-        LOGGER.error("Usage: %s <URL> <architecture>", sys.argv[0])
-        sys.exit(1)
 
-    url, architecture = sys.argv[1:3]
+def init_dirs():
+    os.makedirs(BINARY_DIR, exist_ok=True)
+    os.makedirs(PROFILE_DIR, exist_ok=True)
 
-    index = requests.get(f"{url}/index.json")
 
-    if not index.ok:
-        LOGGER.error("Failed to download data from %s. Is the URL correct ?", url)
-
-    available = index.json()
+def secure_binaries(url, available):
+    """
+    From a dict of {soname, suffix.xz} download and uncompress from the provided URL
+    """
     downloaded = {}
 
-    for library, binary in available.get('binaries', {}).get(architecture, {}).items():
+    for library, binary in available.items():
         bin_url = f"{url}/{binary}"
-        destination = f"{USER_PREFIX}/binaries/{Path(binary).stem}"
+        destination = Path(BINARY_DIR, Path(binary).stem).as_posix()
 
         r = requests.get(bin_url)
 
@@ -50,8 +50,33 @@ if __name__ == '__main__':
             for chunk in r.iter_content(chunk_size=8192):
                 decompressed.write(xz.decompress(chunk))
 
+        st = os.stat(destination)
+        os.chmod(destination, st.st_mode | stat.S_IEXEC)
+
         downloaded[library] = destination
 
-    binary_index = f"{USER_PREFIX}/binaries/index.json"
-    with open(binary_index, "w") as index_json:
+    with open(BINARY_INDEX, "w") as index_json:
         json.dump(downloaded, index_json)
+
+
+if __name__ == '__main__':
+    if getattr(sys, 'frozen', False):
+        __file__ = sys.executable
+
+    if len(sys.argv) < 3:
+        LOGGER.error("Usage: %s <URL> <architecture>", sys.argv[0])
+        sys.exit(1)
+
+    init_dirs()
+
+    url, architecture = sys.argv[1:3]
+
+    index = requests.get(f"{url}/index.json")
+
+    if not index.ok:
+        LOGGER.error("Failed to download data from %s. Is the URL correct ?",
+                     url)
+
+    available = index.json()
+
+    secure_binaries(url, available.get('binaries', {}).get(architecture, {}))
