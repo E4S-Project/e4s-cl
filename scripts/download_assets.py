d30e86ffa8cf954f624a7c20408517d0beceb7b7
Jean-Baptiste Skutnik <jskutnik@uoregon.edu>
2021-11-24 10:16:45 -0800

    Lint and refactor download asset script

--- scripts/download_assets.py
+++ scripts/download_assets.py
@@ -1,86 +1,85 @@
 #!/bin/env python3
+"""
+Script downloading and decompressing precompiled binaries and prebuilt profiles \
+from a given URL and adding them to e4s-cl's database
+"""
 
 import os
 import stat
 import sys
 import lzma
-import json
-import requests
 from pathlib import Path
+from requests import get
 
+# Resolve the e4s-cl installation and add it to the PYTHONPATH
 here = os.path.realpath(os.path.dirname(__file__))
 os.environ['__E4S_CL_HOME__'] = os.path.join(here, '..')
 packages = os.path.join(here, '..', 'packages')
 sys.path.insert(0, packages)
 
 from e4s_cl import logger, USER_PREFIX
-from e4s_cl.cf.assets import add_builtin_profile, add_precompiled_binary
+from e4s_cl.cf.assets import (add_builtin_profile, add_precompiled_binary,
+                              SAMPLE_BINARY_ID)
 
 LOGGER = logger.get_logger(__name__)
 
-BINARY_DIR = Path(USER_PREFIX, "binaries").as_posix()
-PROFILE_DIR = Path(USER_PREFIX, "profiles").as_posix()
-BINARY_INDEX = Path(BINARY_DIR, "index.json").as_posix()
-PROFILE_INDEX = Path(PROFILE_DIR, "index.json").as_posix()
+BINARY_DIR = Path(USER_PREFIX, SAMPLE_BINARY_ID).as_posix()
 
 
 def init_dirs():
     os.makedirs(BINARY_DIR, exist_ok=True)
-    os.makedirs(PROFILE_DIR, exist_ok=True)
 
 
-def secure_binaries(url, available):
+def secure_binaries(url: str, available: dict) -> None:
     """
     From a dict of {soname, suffix.xz} download and uncompress from the provided URL
     """
-    downloaded = {}
-
     for library, binary in available.items():
         bin_url = f"{url}/{binary}"
         destination = Path(BINARY_DIR, Path(binary).stem).as_posix()
 
-        r = requests.get(bin_url)
+        answer = get(bin_url)
 
-        if not r.ok:
-            LOGGER.warn(f"Failed to access {bin_url}")
+        if not answer.ok:
+            LOGGER.warning("Failed to access %s", bin_url)
             continue
 
         with open(destination, "wb") as decompressed:
-            xz = lzma.LZMADecompressor()
+            compressed_data = lzma.LZMADecompressor()
 
-            for chunk in r.iter_content(chunk_size=8192):
-                decompressed.write(xz.decompress(chunk))
+            for chunk in answer.iter_content(chunk_size=8192):
+                decompressed.write(compressed_data.decompress(chunk))
 
-        st = os.stat(destination)
-        os.chmod(destination, st.st_mode | stat.S_IEXEC)
+        status = os.stat(destination)
+        os.chmod(destination, status.st_mode | stat.S_IEXEC)
 
         add_precompiled_binary(library, destination)
 
 
-def secure_profiles(url, available, system):
+def secure_profile(url: str, available: dict, system: str) -> None:
     """
     If a profile url is returned, download and put its JSON file in the profile directory
     """
     if not available or not available.get(system):
+        LOGGER.error("Failed to locate a profile for system '%s', skipping ..", system)
         return
 
     suffix = available.get(system)
 
     profile_url = f"{url}/{suffix}"
-    destination = Path(PROFILE_DIR, Path(suffix).name).as_posix()
-    r = requests.get(profile_url)
+    answer = get(profile_url)
 
-    if not r.ok:
-        LOGGER.warn(f"Failed to access {profile_url}")
+    if not answer.ok:
+        LOGGER.warning("Failed to access %s", profile_url)
         return
 
-    add_builtin_profile(system, r.json())
+    add_builtin_profile(system, answer.json())
 
 
-if __name__ == '__main__':
-    if getattr(sys, 'frozen', False):
-        __file__ = sys.executable
-
+def main():
+    """
+    Check command line, ensure directories are created, query index and download
+    """
     if len(sys.argv) < 3:
         LOGGER.error("Usage: %s <URL> <architecture> [system]", sys.argv[0])
         sys.exit(1)
@@ -92,7 +91,7 @@ if __name__ == '__main__':
     if len(sys.argv) > 3:
         system = sys.argv[3]
 
-    index = requests.get(f"{url}/index.json")
+    index = get(f"{url}/index.json")
 
     if not index.ok:
         LOGGER.error("Failed to download data from %s. Is the URL correct ?",
@@ -101,4 +100,12 @@ if __name__ == '__main__':
     available = index.json()
 
     secure_binaries(url, available.get('binaries', {}).get(architecture, {}))
-    secure_profiles(url, available.get('profiles', {}), system)
+    if system:
+        secure_profile(url, available.get('profiles', {}), system)
+
+
+if __name__ == '__main__':
+    if getattr(sys, 'frozen', False):
+        __file__ = sys.executable
+
+    main()
