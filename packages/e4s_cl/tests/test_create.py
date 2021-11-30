c711d0ef42c873030cbec95770018d4029051341
Jean-Baptiste Skutnik <jskutnik@uoregon.edu>
2021-11-29 18:36:22 -0800

    Removed cross-command testing

--- packages/e4s_cl/tests/test_create.py
+++ packages/e4s_cl/tests/test_create.py
@@ -1,88 +1,117 @@
-import e4s_cl
+"""
+Tests asserting the correct results of the profile create function
+"""
+
 from e4s_cl import tests
-from e4s_cl.cli.commands.profile.create import COMMAND as create_command
-from e4s_cl.cli.commands.profile.list import COMMAND as list_command
-from e4s_cl.cli.commands.profile.show import COMMAND as show_command
+from e4s_cl.model.profile import Profile
+from e4s_cl.cli.commands.profile.create import COMMAND as command
 
 
 class ProfileCreateTest(tests.TestCase):
+    """
+    The actual tests. Profiles are deleted after every test.
+    """
     def setUp(self):
         self.profile_name = 'test_profile'
 
     def tearDown(self):
+        Profile.controller().unselect()
         self.resetStorage()
 
     def test_create(self):
-        stdout, stderr = self.assertCommandReturnValue(0, create_command,
-                                                       [self.profile_name])
-        stdout, stderr = self.assertCommandReturnValue(0, list_command, [])
-        self.assertIn(self.profile_name, stdout)
-
-    def test_create_image(self):
-        stdout, stderr = self.assertCommandReturnValue(0, create_command, [
-            self.profile_name, '--image', '/tmp/e4s_cl/image', '--backend',
-            'technology'
-        ])
-        stdout, stderr = self.assertCommandReturnValue(0, list_command, [])
-        self.assertIn(self.profile_name, stdout)
+        self.assertCommandReturnValue(0, command, [self.profile_name])
+        self.assertTrue(Profile.controller().one({'name': self.profile_name}))
 
     def test_create_libraries(self):
-        libraries = ['/tmp/e4s_cl/lib%d.so' % k for k in range(5)]
+        """
+        Ensure libraries are added to the profile
+        """
+        libraries = [f'/tmp/e4s_cl/lib{no}.so' for no in range(5)]
 
-        stdout, stderr = self.assertCommandReturnValue(
-            0, create_command,
+        self.assertCommandReturnValue(
+            0, command,
             [self.profile_name, '--libraries', ",".join(libraries)])
 
-        stdout, stderr = self.assertCommandReturnValue(0, list_command, [])
-        self.assertIn(self.profile_name, stdout)
+        profile = Profile.controller().one({'name': self.profile_name})
+        self.assertTrue(profile)
 
-        stdout, stderr = self.assertCommandReturnValue(0, show_command,
-                                                       [self.profile_name])
         for libname in libraries:
-            self.assertIn(libname, stdout)
+            self.assertIn(libname, profile.get('libraries', []))
 
     def test_create_files(self):
-        files = ['/tmp/e4s_cl/file%d.txt' % k for k in range(5)]
+        """
+        Ensure files are added to the profile
+        """
+        files = [f'/tmp/e4s_cl/file{no}.txt' for no in range(5)]
 
-        stdout, stderr = self.assertCommandReturnValue(
-            0, create_command, [self.profile_name, '--files', ",".join(files)])
+        self.assertCommandReturnValue(
+            0, command, [self.profile_name, '--files', ",".join(files)])
 
-        stdout, stderr = self.assertCommandReturnValue(0, list_command, [])
-        self.assertIn(self.profile_name, stdout)
+        profile = Profile.controller().one({'name': self.profile_name})
+        self.assertTrue(profile)
 
-        stdout, stderr = self.assertCommandReturnValue(0, show_command,
-                                                       [self.profile_name])
         for filename in files:
-            self.assertIn(filename, stdout)
+            self.assertIn(filename, profile.get('files', []))
 
     def test_create_posix(self):
+        """
+        Ensure paths are formatted to be posix-compliant
+        """
         posix = '/tmp/test.txt'
         non_posix = '/tmp/test.txt///////'
 
-        stdout, stderr = self.assertCommandReturnValue(
-            0, create_command, [self.profile_name, '--files', non_posix])
+        self.assertCommandReturnValue(
+            0, command, [self.profile_name, '--files', non_posix])
 
-        stdout, stderr = self.assertCommandReturnValue(0, list_command, [])
-        self.assertIn(self.profile_name, stdout)
+        profile = Profile.controller().one({'name': self.profile_name})
+        self.assertTrue(profile)
 
-        stdout, stderr = self.assertCommandReturnValue(0, show_command,
-                                                       [self.profile_name])
-        self.assertNotIn(non_posix, stdout)
-        self.assertIn(posix, stdout)
+        files = profile.get('files', [])
+        self.assertNotIn(non_posix, files)
+        self.assertIn(posix, files)
 
     def test_create_wrong_arguments(self):
-        stdout, stderr = self.assertNotCommandReturnValue(
-            0, create_command, [self.profile_name, '--when', 'now'])
+        self.assertNotCommandReturnValue(0, command,
+                                         [self.profile_name, '--when', 'now'])
 
-        stdout, stderr = self.assertCommandReturnValue(0, list_command, [])
-        self.assertNotIn(self.profile_name, stdout)
+        self.assertFalse(Profile.controller().one({'name': self.profile_name}))
 
     def test_unique(self):
-        stdout, stderr = self.assertCommandReturnValue(0, create_command,
-                                                       [self.profile_name])
-        stdout, stderr = self.assertNotCommandReturnValue(
-            0, create_command, [self.profile_name])
+        self.assertCommandReturnValue(0, command, [self.profile_name])
+        _, stderr = self.assertNotCommandReturnValue(0, command,
+                                                     [self.profile_name])
         self.assertIn('profile create <profile_name>', stderr)
         self.assertIn(
-            "profile create: error: A profile with name='%s' already exists" %
-            self.profile_name, stderr)
+            f"profile create: error: A profile with name='{self.profile_name}' already exists",
+            stderr)
+
+
+fields = {
+    ('--image', 'image', 'image:id', 'image_docker_style'),
+    ('--image', 'image', '/path/to/image', 'image_singularity_style'),
+    ('--backend', 'backend', 'singularity', 'backend'),
+    ('--source', 'source', '/path/to/script.sh', 'source'),
+    ('--wi4mpi', 'wi4mpi', '/usr/packages/wi4mpi', 'wi4mpi_location'),
+    ('--wi4mpi_options', 'wi4mpi_options', '"-T mpich -F openmpi"', 'wi4mpi_options'),
+}
+
+
+def wrapper(option, field, argument, test_name):
+    """
+    Generate tests from a simple pattern to ensure all fields are correctly added
+    """
+    def generated_test(self):
+        self.assertCommandReturnValue(0, command,
+                                      [self.profile_name, option, argument])
+        profile = Profile.controller().one({'name': self.profile_name})
+        self.assertTrue(profile)
+        self.assertEqual(argument, profile.get(field))
+
+    generated_test.__name__ = f"test_{test_name}"
+
+    return generated_test
+
+
+for arguments in fields:
+    test = wrapper(*arguments)
+    setattr(ProfileCreateTest, test.__name__, test)
