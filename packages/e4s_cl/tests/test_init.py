314c7a3bfe7d8b86031de11f8acd714568b79e20
Jean-Baptiste Skutnik <jskutnik@uoregon.edu>
2021-11-30 10:19:42 -0800

    Improved init tests

--- packages/e4s_cl/tests/test_init.py
+++ packages/e4s_cl/tests/test_init.py
@@ -1,11 +1,20 @@
-import e4s_cl
+"""
+Tests ensuring the init command behaves as intented
+"""
+
+from itertools import combinations
 from e4s_cl import tests
+from e4s_cl.model.profile import Profile
 from e4s_cl.cf.assets import add_builtin_profile, remove_builtin_profile
 from e4s_cl.cli.commands.init import COMMAND as command
 
 TEST_SYSTEM = '__test_system'
 
+
 class InitTest(tests.TestCase):
+    """
+    Partial class definition: more tests are defined below
+    """
     def setUp(self):
         add_builtin_profile(TEST_SYSTEM, {'name': TEST_SYSTEM})
 
@@ -14,3 +23,45 @@ class InitTest(tests.TestCase):
 
     def test_system(self):
         self.assertCommandReturnValue(0, command, f"--system {TEST_SYSTEM}")
+        self.assertEqual(Profile.controller().selected().get('name'),
+                         TEST_SYSTEM)
+
+    def test_wi4mpi(self):
+        self.assertCommandReturnValue(
+            0, command,
+            "--wi4mpi /path/to/installation --wi4mpi_options '-T to -F from'")
+        profile = Profile.controller().selected()
+
+        self.assertTrue(profile)
+        self.assertEqual(profile.get('wi4mpi'), '/path/to/installation')
+        self.assertEqual(profile.get('wi4mpi_options'), '-T to -F from')
+
+
+groups = [[('--system', TEST_SYSTEM)],
+          [
+              ('--wi4mpi', '/path/to/installation'),
+              ('--wi4mpi_options', "'-T to -F from'"),
+          ], [
+              ('--mpi', '/path/to/installation'),
+              ('--launcher', '/path/to/binary'),
+              ('--launcher_args', "'-np 8192'"),
+          ]]
+
+def wrapper(option1, value1, option2, value2):
+    """
+    Generate tests from a simple pattern to ensure all fields are correctly added
+    """
+    def generated_test(self):
+        self.assertNotCommandReturnValue(0, command,
+                                      [option1, value1, option2, value2])
+
+    generated_test.__name__ = f"test_{option1.strip('-')}_{option2.strip('-')}"
+
+    return generated_test
+
+
+for conflicting_left, conflicting_right in combinations(groups, 2):
+    for argument1 in conflicting_left:
+        for argument2 in conflicting_right:
+            test = wrapper(*argument1, *argument2)
+            setattr(InitTest, test.__name__, test)
