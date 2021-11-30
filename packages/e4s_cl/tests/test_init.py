e103cf3125432aef4a3cd9460451d743b95cb65d
Jean-Baptiste Skutnik <jskutnik@uoregon.edu>
2021-11-30 11:21:57 -0800

    Added skippable init test

--- packages/e4s_cl/tests/test_init.py
+++ packages/e4s_cl/tests/test_init.py
@@ -2,11 +2,12 @@
 Tests ensuring the init command behaves as intented
 """
 
+from os import getenv
 from itertools import combinations
 from e4s_cl import tests
 from e4s_cl.model.profile import Profile
 from e4s_cl.cf.assets import add_builtin_profile, remove_builtin_profile
-from e4s_cl.cli.commands.init import COMMAND as command
+from e4s_cl.cli.commands.init import COMMAND
 
 TEST_SYSTEM = '__test_system'
 
@@ -20,15 +21,17 @@ class InitTest(tests.TestCase):
 
     def tearDown(self):
         remove_builtin_profile(TEST_SYSTEM)
+        Profile.controller().unselect()
+        self.resetStorage()
 
     def test_system(self):
-        self.assertCommandReturnValue(0, command, f"--system {TEST_SYSTEM}")
+        self.assertCommandReturnValue(0, COMMAND, f"--system {TEST_SYSTEM}")
         self.assertEqual(Profile.controller().selected().get('name'),
                          TEST_SYSTEM)
 
     def test_wi4mpi(self):
         self.assertCommandReturnValue(
-            0, command,
+            0, COMMAND,
             "--wi4mpi /path/to/installation --wi4mpi_options '-T to -F from'")
         profile = Profile.controller().selected()
 
@@ -36,6 +39,25 @@ class InitTest(tests.TestCase):
         self.assertEqual(profile.get('wi4mpi'), '/path/to/installation')
         self.assertEqual(profile.get('wi4mpi_options'), '-T to -F from')
 
+    def test_rename_system(self):
+        self.assertCommandReturnValue(0, COMMAND, f"--profile init_test_profile --system {TEST_SYSTEM}")
+        self.assertEqual(Profile.controller().selected().get('name'),
+                         'init_test_profile')
+
+    def test_rename_wi4mpi(self):
+        self.assertCommandReturnValue(
+            0, COMMAND,
+            "--profile init_test_profile --wi4mpi /path/to/installation --wi4mpi_options '-T to -F from'")
+        profile = Profile.controller().selected()
+
+        self.assertTrue(profile)
+        self.assertEqual(profile.get('name'), 'init_test_profile')
+        self.assertEqual(profile.get('wi4mpi'), '/path/to/installation')
+        self.assertEqual(profile.get('wi4mpi_options'), '-T to -F from')
+
+    @tests.skipIf(not getenv('__E4S_CL_TEST_INIT'), "Init test from environment disabled")
+    def test_init_environment(self):
+        self.assertCommandReturnValue(0, COMMAND, [])
 
 groups = [[('--system', TEST_SYSTEM)],
           [
@@ -52,7 +74,7 @@ def wrapper(option1, value1, option2, value2):
     Generate tests from a simple pattern to ensure all fields are correctly added
     """
     def generated_test(self):
-        self.assertNotCommandReturnValue(0, command,
+        self.assertNotCommandReturnValue(0, COMMAND,
                                       [option1, value1, option2, value2])
 
     generated_test.__name__ = f"test_{option1.strip('-')}_{option2.strip('-')}"
