1be31ec4be1c6abe834d1e6289a4a0954d523c04
Jean-Baptiste Skutnik <jskutnik@uoregon.edu>
2022-02-14 14:37:13 -0800

    Docker backend poc

--- packages/e4s_cl/cf/containers/docker.py
+++ packages/e4s_cl/cf/containers/docker.py
@@ -1,6 +1,9 @@
+import os
 import sys
 import docker
 from e4s_cl import logger
+from e4s_cl.variables import CHILD_MARKER
+from e4s_cl.cf.pipe import ENV_VAR_NAMED, NamedPipe
 from e4s_cl.cf.containers import Container, FileOptions
 
 LOGGER = logger.get_logger(__name__)
@@ -10,11 +13,20 @@ EXECUTABLES = ['docker']
 MIMES = []
 
 
+
 class DockerContainer(Container):
+    pipe_manager = NamedPipe
 
     def run(self, command: list[str]):
         # Create the client from the environment
         client = docker.from_env()
+
+        for variable in [ENV_VAR_NAMED, CHILD_MARKER, logger.LOG_ID_MARKER]:
+            if os.environ.get(variable, ''):
+                self.bind_env_var(variable, os.environ[variable])
+
+        if os.environ.get(ENV_VAR_NAMED, ''):
+            self.bind_file(os.environ[ENV_VAR_NAMED], option=FileOptions.READ_WRITE)
 
         # Ensure the queried image is accessible
         try:
@@ -33,8 +45,16 @@ class DockerContainer(Container):
                     type='bind',
                     read_only=(options_val == FileOptions.READ_ONLY)))
 
+        container_env = dict(os.environ)
+        for key, val in self.env.items():
+            if val is None:
+                container_env.pop(key, None)
+            else:
+                container_env[key] = val
+
         outlog = client.containers.run(image,
                                        command,
+                                       environment=container_env,
                                        stdout=True,
                                        stderr=True,
                                        mounts=mounts)
