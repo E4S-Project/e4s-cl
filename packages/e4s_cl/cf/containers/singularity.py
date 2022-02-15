16ca562131458d4dc181af5385cf0e1438e30b02
Jean-Baptiste Skutnik <jskutnik@uoregon.edu>
2022-02-15 15:26:12 -0800

    Refactor singularity module

--- packages/e4s_cl/cf/containers/singularity.py
+++ packages/e4s_cl/cf/containers/singularity.py
@@ -37,29 +37,7 @@ class SingularityContainer(Container):
         #self.bind_file('/dev', option=FileOptions.READ_WRITE)
         #self.bind_file('/tmp', option=FileOptions.READ_WRITE)
 
-    def _prepare(self, command) -> list[str]:
-        self.add_ld_library_path("/.singularity.d/libs")
-        self.env.update(
-            {'SINGULARITYENV_LD_PRELOAD': ":".join(self.ld_preload)})
-        self.env.update(
-            {'SINGULARITYENV_LD_LIBRARY_PATH': ":".join(self.ld_lib_path)})
-        self.format_bound()
-        nvidia_flag = ['--nv'] if self._has_nvidia() else []
-
-        return [
-            self.executable, 'exec', *self._working_dir(), *nvidia_flag,
-            self.image, *command
-        ]
-
-    def run(self, command):
-        if not which(self.executable):
-            raise BackendNotAvailableError(self.executable)
-
-        container_cmd = self._prepare(command)
-
-        return run_subprocess(container_cmd, env=self.env)
-
-    def format_bound(self):
+    def _format_bound(self):
         """
         Format a list of files to a compatible bind option of singularity
         """
@@ -70,6 +48,20 @@ class SingularityContainer(Container):
 
         self.env.update({"SINGULARITY_BIND": ','.join(_format())})
 
+    def _prepare(self, command) -> list[str]:
+        self.add_ld_library_path("/.singularity.d/libs")
+        self.env.update(
+            {'SINGULARITYENV_LD_PRELOAD': ":".join(self.ld_preload)})
+        self.env.update(
+            {'SINGULARITYENV_LD_LIBRARY_PATH': ":".join(self.ld_lib_path)})
+        self._format_bound()
+        nvidia_flag = ['--nv'] if self._has_nvidia() else []
+
+        return [
+            self.executable, 'exec', *self._working_dir(), *nvidia_flag,
+            self.image, *command
+        ]
+
     def bind_env_var(self, key, value):
         self.env.update({f"SINGULARITYENV_{key}": value})
 
@@ -78,6 +70,15 @@ class SingularityContainer(Container):
             LOGGER.debug("Disabling Nvidia support: no libraries found")
             return False
         return True
+
+    def run(self, command):
+        if not which(self.executable):
+            raise BackendNotAvailableError(self.executable)
+
+        container_cmd = self._prepare(command)
+
+        return run_subprocess(container_cmd, env=self.env)
+
 
 
 CLASS = SingularityContainer
