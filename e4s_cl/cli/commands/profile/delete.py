31f41ed040b6f62f7a43ad3e3544367b1439b257
Frederick Deny <fdeny@uoregon.edu>
2022-04-05 14:57:41 -0700

    updated profile delete documentation for wildcard capabilities

--- e4s_cl/cli/commands/profile/delete.py
+++ e4s_cl/cli/commands/profile/delete.py
@@ -1,5 +1,43 @@
 """
 Delete the profile associated with the name passed as an argument.
+
+
+Delete Multiple Profiles
+----------------------
+
+You can delete multiple profiles at once by using the '*' wildcard, which will match with any single or multiple characters. 
+
+
+.. code::
+
+    $ e4s-cl profile list 
+    ============================= Profile Configurations (/home/fdeny/.local/e4s_cl/user.json) ==
+    +----------+---------------+---------+-------+-----------+-------+--------+
+    | Selected |     Name      | Backend | Image | Libraries | Files | WI4MPI |
+    +==========+===============+=========+=======+===========+=======+========+
+    |    *     |   MPICH_3.4.2 |    None |  None |    16     |   1   |   No   |
+    +----------+---------------+---------+-------+-----------+-------+--------+
+    |          | MPICH_3.4.2-2 |    None |  None |    16     |   1   |   No   |
+    +----------+---------------+---------+-------+-----------+-------+--------+
+    |          |   MPICH_3.3.2 |    None |  None |    16     |   1   |   No   |
+    +----------+---------------+---------+-------+-----------+-------+--------+
+    |          | MPICH_3.3.2-2 |    None |  None |    16     |   1   |   No   |
+    +----------+---------------+---------+-------+-----------+-------+--------+
+
+    $ e4s-cl profile delete MPICH_3.*-2 
+    Deleted profile 'MPICH_3.4.2-2'
+    Deleted profile 'MPICH_3.3.2-2'
+
+    $ e4s-cl profile list
+    ============================= Profile Configurations (/home/fdeny/.local/e4s_cl/user.json) ==
+    +----------+-------------+---------+-------+-----------+-------+--------+
+    | Selected |    Name     | Backend | Image | Libraries | Files | WI4MPI |
+    +==========+=============+=========+=======+===========+=======+========+
+    |    *     | MPICH_3.4.2 |    None |  None |    16     |   1   |   No   |
+    +----------+-------------+---------+-------+-----------+-------+--------+
+    |          | MPICH_3.3.2 |    None |  None |    16     |   1   |   No   |
+    +----------+-------------+---------+-------+-----------+-------+--------+
+
 """
 
 from e4s_cl.cli.cli_view import DeleteCommand
