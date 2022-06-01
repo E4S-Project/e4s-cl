8dd091b6ad5a2558405b2fbde9e757eeaab01006
Frederick Deny <fdeny@uoregon.edu>
2022-06-01 13:09:06 -0700

    added configuration translation table dictionary

--- e4s_cl/config.py
+++ e4s_cl/config.py
@@ -1,6 +1,13 @@
 import yaml
 
+confxGlobal = {'container directory': 'CONTAINER_DIR'}
+
 class Configuration:
+    updated_globals = dict()
+
     def __init__(self, confFile):
         with open(confFile) as f:
             self.data = yaml.safe_load(f)
+        for key in self.data.keys():
+            global_key = confxGlobal[key]
+            self.updated_globals.update({confxGlobal[key]: self.data[key]})
