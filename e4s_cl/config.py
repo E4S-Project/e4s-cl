34bb4f6146b1f67554cdbf4349882b5d430d3b9d
Frederick Deny <fdeny@uoregon.edu>
2022-07-18 15:52:41 -0700

    rework of configuration class and implemented flatten function

--- e4s_cl/config.py
+++ e4s_cl/config.py
@@ -12,20 +12,30 @@ default_config_path = user_home + "/.config/e4s-cl.yaml"
 alternate_config_path = "/etc/e4s-cl/e4s-cl.yaml"
 configuration_file = ""
 
+def flatten(data):
+    flat = dict()
+    for key in data.keys():
+        value = data.get(key)
+        if isinstance(value, str):
+            flat.update({str(key): value})
+        elif isinstance(value, dict):
+            for value_key in value:
+                flat.update({'.'.join((str(key),str(value_key))): value.get(value_key)})
+    return flat
+
 class Configuration:
-    updated_globals = dict()
+    raw_data = dict()
 
     def __init__(self, confFile):
         if confFile:
             with open(confFile) as f:
                 self.data = yaml.safe_load(f)
             for key in self.data.keys():
-                global_key = confxGlobal[key]
-                self.updated_globals.update({confxGlobal[key]: self.data[key]})
-
+                self.raw_data.update({confxGlobal[key]: self.data[key]})
+    
     def options(self, option, sub_option=None):
-        if self.updated_globals and self.updated_globals.get(option):
-            value = self.updated_globals.get(option)
+        if self.raw_data and self.raw_data.get(option):
+            value = self.raw_data.get(option)
             if isinstance(value, bool):
                 return value
             elif isinstance(value, dict):
@@ -41,6 +51,6 @@ elif exists(alternate_config_path):
 CONFIGURATION_VALUES = None
 
 if CONFIGURATION_VALUES is None and configuration_file:
-    CONFIGURATION_VALUES = Configuration(configuration_file).updated_globals
+    CONFIGURATION_VALUES = Configuration(configuration_file).raw_data
 
 configuration = Configuration(configuration_file)
--- tests/test_config.py
+++ tests/test_config.py
@@ -9,33 +9,61 @@ from io import StringIO
 from pathlib import Path
 from unittest.mock import patch
 import e4s_cl.config
+from e4s_cl.config import flatten
 from e4s_cl.variables import set_dry_run
 from e4s_cl.cf.containers import Container
 from e4s_cl.cli.commands.launch import COMMAND as launch_command
 
-configuration_file = Path(Path(__file__).parent, "assets","e4s-cl.yaml").as_posix()
+configuration_file = Path(Path(__file__).parent, "assets",
+                          "e4s-cl.yaml").as_posix()
 configuration = e4s_cl.config.Configuration(configuration_file)
+
 
 class ConfigTest(tests.TestCase):
     """Unit tests for e4s-cl's configuration file use"""
+
     def test_container_dir(self):
-        self.assertEqual(configuration.updated_globals.get('CONTAINER_DIR'), "/diffdirectory")
+        self.assertEqual(configuration.raw_data.get('CONTAINER_DIR'),
+                         "/diffdirectory")
 
     def test_configuration_options(self):
-        self.assertEqual(configuration.options('LAUNCHER_OPTIONS'), ['-n', '8'])
-        self.assertEqual(configuration.options('SINGULARITY_OPTIONS', 'cli_options'), ['--hostname', 'diffname'])
+        self.assertEqual(configuration.options('LAUNCHER_OPTIONS'),
+                         ['-n', '8'])
+        self.assertEqual(
+            configuration.options('SINGULARITY_OPTIONS', 'cli_options'),
+            ['--hostname', 'diffname'])
 
         self.assertEqual(configuration.options(None), [])
-        
+
         self.assertEqual(configuration.options('OPTION_NONE'), [])
 
-    @patch('sys.stdout', new_callable = StringIO)
+    @patch('sys.stdout', new_callable=StringIO)
     def test_configured_launch(self, stdout):
         set_dry_run(True)
-        launch_command.main(shlex.split(f"--backend containerless --image None mpirun hostname"))
+        launch_command.main(
+            shlex.split(
+                f"--backend containerless --image None mpirun hostname"))
         self.assertIn('-n 8', stdout.getvalue())
 
     def test_configured_sing_container(self):
         container = Container(name='singularity')
         command = ['']
         self.assertIn('diffname', container._prepare(command))
+
+    def test_flat(self):
+        data = {
+            'container directory': '/diffdirectory',
+            'launcher options': '-n 8',
+            'singularity': {
+                'cli_options': '--hostname diffname',
+                'build_options': '-s'
+            }
+        }
+        flat_data = {
+            'container directory': '/diffdirectory',
+            'launcher options': '-n 8',
+            'singularity.cli_options': '--hostname diffname',
+            'singularity.build_options': '-s'  
+        }
+        self.assertEqual(flatten(data), flat_data)
+
