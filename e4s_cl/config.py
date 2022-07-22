7f1b2800dcd0f27cd57b0935b314f22be8d5c38b
Jean-Baptiste Skutnik <jskutnik@uoregon.edu>
2022-07-22 09:20:45 -0700

    Recursive configuration parsing

--- e4s_cl/config.py
+++ e4s_cl/config.py
@@ -2,29 +2,30 @@ import e4s_cl
 from dataclasses import dataclass
 import yaml
 from pathlib import Path
-from os.path import expanduser, exists
 
-user_home = expanduser('~')
-default_config_path = user_home + "/.config/e4s-cl.yaml"
-alternate_config_path = "/etc/e4s-cl/e4s-cl.yaml"
-configuration_file = ""
+DEFAULT_CONFIG_PATH = Path.home() / "/.config/e4s-cl.yaml"
+ALTERNATE_CONFIG_PATH = "/etc/e4s-cl/e4s-cl.yaml"
 
 
 def flatten(data):
     SEPARATOR = '_'
-    flat = dict()
 
-    for key in data.keys():
-        value = data.get(key)
-        if isinstance(value, str):
-            flat.update({str(key): value})
-        elif isinstance(value, dict):
-            for value_key in value:
-                flat.update({
-                    SEPARATOR.join((str(key), str(value_key))):
-                    value.get(value_key)
-                })
-    return flat
+    def _p(prefix, string):
+        return str(SEPARATOR.join(filter(None, [prefix, string])))
+
+    def _intermediate(prefix, data: dict):
+        flat = dict()
+
+        for key, value in data.items():
+            if isinstance(value, dict):
+                for ckey, cval in _intermediate(str(key), value).items():
+                    flat.update({_p(prefix, ckey): cval})
+            else:
+                flat.update({_p(prefix, key): str(value)})
+
+        return flat
+
+    return _intermediate('', data)
 
 
 @dataclass(frozen=True)
@@ -94,5 +95,6 @@ class Configuration:
 
         return Configuration(self._fields | rhs._fields)
 
+
 CONFIGURATION = Configuration.default | Configuration.create_from(
-    alternate_config_path) | Configuration.create_from(default_config_path)
+    ALTERNATE_CONFIG_PATH) | Configuration.create_from(DEFAULT_CONFIG_PATH)
