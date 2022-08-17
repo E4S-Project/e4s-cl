bc3f4fc79a660d3d8db5e841438eccb9943a079e
Frederick Deny <fdeny@uoregon.edu>
2022-08-17 15:05:07 -0700

    refactored code for lisibility

--- e4s_cl/logger.py
+++ e4s_cl/logger.py
@@ -452,21 +452,15 @@ def add_file_handler(
 
     return True
 
-def symlink_latest():
+
+def update_symlink_latest():
     if os.path.exists(Path(LOG_FILE.parent, LOG_ID)):
         try:
             os.unlink(LOG_LATEST)
-        except OSError as err:
-            _ROOT_LOGGER.debug("Unlink %s failed: %s", LOG_LATEST.as_posix(),
-                               str(err))
-
-        try:
             os.symlink(Path(LOG_FILE.parent, LOG_ID), LOG_LATEST)
         except OSError as err:
-            _ROOT_LOGGER.debug("Symlink %s failed: %s", LOG_LATEST.as_posix(),
-                               str(err))
-        else:
-            os.environ[LOG_ID_MARKER] = LOG_LATEST.name
+            _ROOT_LOGGER.debug("Updating symlink %s failed: %s",
+                               LOG_LATEST.as_posix(), str(err))
 
 
 _ROOT_LOGGER = logging.getLogger()
@@ -507,9 +501,9 @@ if is_parent():
                              fmt=f"%(asctime)s\t{LOG_ID}\t%(message)s"))
         index_logger.info(" ".join(sys.argv))
 
-        # Create a symlink towards the latest log directory at the end
-        # of the program execution
-        atexit.register(symlink_latest)
+        # Registers the update of the latest logs symlink to be run
+        # at the end of the execution
+        atexit.register(update_symlink_latest)
 
     # pylint: disable=logging-not-lazy
     _ROOT_LOGGER.debug(
