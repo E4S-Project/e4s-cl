8de1fcbb8503dadbeccb3089c2c01b43d0227aef
Jean-Baptiste Skutnik <jskutnik@uoregon.edu>
2022-09-12 10:16:27 -0700

    Case insensitive column selection

--- e4s_cl/cli/commands/profile/list.py
+++ e4s_cl/cli/commands/profile/list.py
@@ -33,9 +33,79 @@ Pass option **-s** to print only the names and disable formatting.
 
 """
 
+from typing import List, Dict
+from e4s_cl.logger import get_logger
 from e4s_cl.cli.cli_view import ListCommand
 from e4s_cl.model.profile import Profile
 from e4s_cl.config import CONFIGURATION
+
+LOGGER = get_logger(__name__)
+
+
+def _count(attr):
+    return lambda x: len(x.get(attr, []))
+
+
+def _selected(attr):
+    return lambda x: '*' if Profile.selected().get(attr) == x[attr] else ' '
+
+
+def _wi4mpi():
+
+    def __defined(profile):
+        if profile.get('wi4mpi') and profile.get('wi4mpi_options'):
+            return "Yes"
+        return "No"
+
+    return __defined
+
+
+DEFINED_DASHBOARD_COLUMNS = [{
+    'header': 'Selected',
+    'function': _selected('name')
+}, {
+    'header': 'Name',
+    'value': 'name',
+    'align': 'r'
+}, {
+    'header': 'Backend',
+    'value': 'backend',
+    'align': 'r'
+}, {
+    'header': 'Image',
+    'value': 'image',
+    'align': 'r'
+}, {
+    'header': 'Libraries',
+    'function': _count('libraries')
+}, {
+    'header': 'Files',
+    'function': _count('files')
+}, {
+    'header': 'WI4MPI',
+    'function': _wi4mpi()
+}]
+
+DEFAULT_COLUMNS = ["selected", "name", "backend", "image"]
+
+
+def _valid_columns(l: List[str]) -> List[Dict]:
+    """
+    Return a list of column definitions as requested by the entry list of column names
+    """
+
+    def column(name):
+        matches = list(
+            filter(lambda x: x['header'].lower() == name.lower(),
+                   DEFINED_DASHBOARD_COLUMNS))
+        if matches:
+            return matches[0]
+
+        LOGGER.warning("Configuration error: Unrecognized column name: %s",
+                       name)
+        return None
+
+    return list(filter(None, map(column, l)))
 
 
 class ProfileListCommand(ListCommand):
@@ -45,62 +115,13 @@ class ProfileListCommand(ListCommand):
     """
 
     def __init__(self):
+        selected_columns = _valid_columns(DEFAULT_COLUMNS)
 
-        def _count(attr):
-            return lambda x: len(x.get(attr, []))
+        config_columns = _valid_columns(CONFIGURATION.profile_list_columns)
+        if config_columns:
+            selected_columns = config_columns
 
-        def _selected(attr):
-            return lambda x: '*' if Profile.selected().get(attr) == x[
-                attr] else ' '
-
-        def _wi4mpi():
-
-            def __defined(profile):
-                if profile.get('wi4mpi') and profile.get('wi4mpi_options'):
-                    return "Yes"
-                return "No"
-
-            return __defined
-
-        all_dashboard_columns = [{
-            'header': 'Selected',
-            'function': _selected('name')
-        }, {
-            'header': 'Name',
-            'value': 'name',
-            'align': 'r'
-        }, {
-            'header': 'Backend',
-            'value': 'backend',
-            'align': 'r'
-        }, {
-            'header': 'Image',
-            'value': 'image',
-            'align': 'r'
-        }, {
-            'header': 'Libraries',
-            'function': _count('libraries')
-        }, {
-            'header': 'Files',
-            'function': _count('files')
-        }, {
-            'header': 'WI4MPI',
-            'function': _wi4mpi()
-        }]
-
-        default_columns = ["Selected", "Name", "Backend", "Image"]
-
-        config_options = CONFIGURATION.profile_list_columns
-        if config_options:
-            default_columns = config_options
-
-        dashboard_columns = list(
-            filter(lambda d: d['header'] in default_columns,
-                   all_dashboard_columns))
-
-        super().__init__(Profile,
-                         __name__,
-                         dashboard_columns=dashboard_columns)
+        super().__init__(Profile, __name__, dashboard_columns=selected_columns)
 
 
 COMMAND = ProfileListCommand()
