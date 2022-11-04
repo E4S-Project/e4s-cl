410bb2e6601d9b90243a487ad7f7d2dabd8ba04c
Jean-Baptiste Skutnik <jskutnik@uoregon.edu>
2022-11-04 13:11:39 -0700

    Prepare release v1.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Jean-Baptiste Skutnik'
 author = 'Jean-Baptiste Skutnik'
 
 # The full version, including alpha/beta/rc tags
-# release = '1.0'
+version = release = '1.0'
 
 # -- General configuration ---------------------------------------------------
 
--- e4s_cl/version.py
+++ e4s_cl/version.py
@@ -1,6 +1,7 @@
 PACKAGE = "e4s_cl"
 VERSION = (1, 0)
-RELEASE_CANDIDATE = 6
-__version__ = '.'.join(map(str, VERSION)) + (f"rc{RELEASE_CANDIDATE}" if RELEASE_CANDIDATE else '')
+RELEASE_CANDIDATE = 0
+__version__ = '.'.join(map(
+    str, VERSION)) + (f"rc{RELEASE_CANDIDATE}" if RELEASE_CANDIDATE else '')
 WEBSITE = 'https://e4s-cl.readthedocs.io'
 LICENSE = 'MIT'
