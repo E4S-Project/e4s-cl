5a5dc7532100594a7aab16d9a28adeb633873adb
Jean-Baptiste Skutnik <jskutnik@uoregon.edu>
2023-03-31 15:40:18 -0700

    Install script alongside project

--- e4s_cl/scripts/e4s_cl_mpi_tester.py
+++ e4s_cl/scripts/e4s_cl_mpi_tester.py
@@ -1,8 +1,38 @@
 #!/usr/bin/env python3
 
+import logging
 import ctypes
+from sys import argv
 from pathlib import Path
+from argparse import ArgumentParser
 from sotools.linker import resolve
+
+DESCRIPTION = """This script will dynamically load an MPI library for the family passed as an argument and run a simple program with it. This serves no purpose by itself and is meant to be traced to list the files an arbitrary MPI library requires to run."""
+EPILOG = """Please report any issues to http://github.com/E4S-Project/e4s-cl."""
+
+PARSER = ArgumentParser(
+    prog=argv[0],
+    description=DESCRIPTION,
+    epilog=EPILOG,
+)
+
+PARSER.add_argument(
+    "family",
+    choices=['openmpi', 'mpich'],
+    help="The MPI family to search for.",
+)
+
+PARSER.add_argument(
+    "-v",
+    "--verbose",
+    action="store_true",
+    help="Trace resolving attempts while searching for the library",
+)
+
+SONAMES = {
+    "openmpi": "libmpi.so.40",
+    "mpich": "libmpi.so.12",
+}
 
 
 class MPIhandles:
@@ -10,6 +40,7 @@ class MPIhandles:
 
 
 def bind_mpich(libpath: Path) -> object:
+    logging.debug("Creating bindings using MPICH library '%s'", libpath)
     lib = ctypes.CDLL(libpath)
 
     MPI = MPIhandles()
@@ -29,6 +60,7 @@ def bind_mpich(libpath: Path) -> object:
 
 
 def bind_ompi(libpath: Path) -> object:
+    logging.debug("Creating bindings using OpenMPI library '%s'", libpath)
     lib = ctypes.CDLL(libpath)
 
     MPI = MPIhandles()
@@ -47,8 +79,33 @@ def bind_ompi(libpath: Path) -> object:
     return MPI
 
 
-if __name__ == "__main__":
-    MPI = bind_ompi(resolve("libmpi.so.40"))
+def main():
+    args = PARSER.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format="%(message)s",
+        )
+
+    # The below line could be expanded to target multiple sonames
+    soname = SONAMES.get(args.family)
+
+    library = resolve(soname)
+    if library is None:
+        logging.error(
+            "Failed to locate required library '%s': is the module loaded ?",
+            soname)
+        exit(1)
+
+    try:
+        if args.family == "openmpi":
+            MPI = bind_ompi(library)
+        elif args.family == "mpich":
+            MPI = bind_mpich(library)
+    except AttributeError as err:
+        logging.error("Binding creation failed: %s", err)
+        exit(1)
 
     world_size = ctypes.c_int()
     world_rank = ctypes.c_int()
@@ -83,3 +140,7 @@ if __name__ == "__main__":
 
     MPI.Barrier(MPI.COMM_WORLD)
     MPI.Finalize()
+
+
+if __name__ == "__main__":
+    main()
